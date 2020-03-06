7e2e6491c4d33bd7e4d498190920ffeddfdce1f5
Tom Swales <tomswales@creativity.technology>
2020-03-06 21:20:26 +0100

    Github release version

---
+++ docs/precache-manifest.12e64e5f3acb62631f0dbe455a1f63a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e316a4fe203333ee5a6c5115ad7f392",
+    "url": "./index.html"
+  },
+  {
+    "revision": "763ce00547574e378ba7",
+    "url": "./static/js/2.0ffbf3f6.chunk.js"
+  },
+  {
+    "revision": "fc845f98b789f2ae8392d194992cbfaa",
+    "url": "./static/js/2.0ffbf3f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58485b96f7b3a8351950",
+    "url": "./static/js/main.d66ea968.chunk.js"
+  },
+  {
+    "revision": "58f60a16ca76246eeeeb",
+    "url": "./static/js/runtime-main.9ffd554d.js"
+  }
+]);
