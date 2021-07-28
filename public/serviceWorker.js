c7a5e43d240924bf2f1d61659f782b980dbf7956
tom <tom@taess.fritz.box>
2021-07-28 22:43:07 +0200

    Updated font

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -6,14 +6,13 @@ self.addEventListener('install', function(event) {
         return cache.addAll(
           [
             './',
-            './serviceWorker.js',
             'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/webfonts/fa-regular-400.woff2',
             './styles.css',
             'https://unpkg.com/purecss@1.0.0/build/pure-min.css',
             'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css',
             'https://fonts.googleapis.com/css?family=PT+Sans',
             './static/js/2.0ffbf3f6.chunk.js',
-            'https://tomswales.github.io/saving_analyst/static/js/main.0bfcedb6.chunk.js'
+            './static/js/main.0bfcedb6.chunk.js'
           ]
         );
       })
